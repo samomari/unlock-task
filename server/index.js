8160d24d9a8b578501020037cf9d8fcfb87210f7
samomari <marius.sam98@gmail.com>
2026-02-23 15:20:30 +0200

    Implement /ask endpoint

--- server/index.js
+++ server/index.js
@@ -7,8 +7,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.post("/ask", (req, res) => {
+  const { question } = req.body;
+
+  if (!question) {
+    return res.status(400).json({ error: "No question provided" });
+  }
+
+  const answers = ["Yes", "No", "Maybe", "I don't know", "Ask again later"];
+  const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+  res.json({ answer: randomAnswer });
 });
 
 app.listen(PORT, () => {
